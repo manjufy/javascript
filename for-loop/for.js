41562685f2caa6015dd157ae9a977a6735b30a1e
Manjunath Reddy <manju16832003@gmail.com>
2018-08-18 20:04:39 +0800

    Explore for loop

--- for-loop/for.js
+++ for-loop/for.js
@@ -14,3 +14,16 @@ for(let i= 0; i < 2; i++) {
   i++
 }
 
+for(const i= 0; i < 2; i++) { 
+  setTimeout(() => console.log(i)) // whats the output
+  i++
+}
+
+
+for(
+  let i = (setTimeout(() => console.log(i)), 0);
+  i < 2;
+  i++
+) {
+  i++
+}
