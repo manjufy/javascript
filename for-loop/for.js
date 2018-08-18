99d36b5947dd6c6423e66e670091a044eba8ca96
Manjunath Reddy <manju16832003@gmail.com>
2018-08-18 19:37:47 +0800

    Explore for loop

---
+++ for-loop/for.js
@@ -0,0 +1,16 @@
+// Exploring for-loop
+
+for(let i= 0; i < 2; i++) {
+   setTimeout(() => console.log(i)) // whats the output
+}
+
+for(let i= 0; i < 2; i++) { 
+  setTimeout(() => console.log(i)) // whats the output
+}
+
+
+for(let i= 0; i < 2; i++) { 
+  setTimeout(() => console.log(i)) // whats the output
+  i++
+}
+
