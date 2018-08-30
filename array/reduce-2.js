3b42aec3c4fb95c2649e7ef2f52cce8053482529
Manjunath Reddy <manju16832003@gmail.com>
2018-08-30 21:16:36 +0800

    More reduce examples

---
+++ array/reduce-2.js
@@ -0,0 +1,46 @@
+const users = [
+    {
+        firstName: 'Bob',
+        lastName: 'Doe',
+        age: 37,
+    }, {
+        firstName: 'Rita',
+        lastName: 'Smith',
+        age: 21,
+    }, {
+        firstName: 'Rick',
+        lastName: 'Fish',
+        age: 28,
+    }, {
+        firstName: 'Betty',
+        lastName: 'Bird',
+        age: 44,
+    }, {
+        firstName: 'Joe',
+        lastName: 'Grover',
+        age: 22,
+    }, {
+        firstName: 'Jill',
+        lastName: 'Pill',
+        age: 19,
+    }, {
+        firstName: 'Sam',
+        lastName: 'Smith',
+        age: 22,
+    }
+];
+
+/**
+ * Get 
+ * - Users full name (first name + last name)
+ * - Only if they are in their 20's
+ * - Only if their full name is 10 characters or longers
+ */
+
+ const twentySomethingLongFullNames = users
+                                            .filter(user => user.age >= 20 && user.age < 30)
+                                            .map(user => `${user.firstName} ${user.lastName}`)
+                                            .filter(fullName => fullName.length >=10)
+console.log(twentySomethingLongFullNames)
+
+// How to break them down
