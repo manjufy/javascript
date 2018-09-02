035d6cbeee62f724f2cbe92f61d1ad5167bc57ca
Manjunath Reddy <manju16832003@gmail.com>
2018-09-03 00:37:22 +0800

    Add comments

--- query-string-to-json.js
+++ query-string-to-json.js
@@ -1,3 +1,7 @@
+/**
+ * Conver the query string to Object format
+ */
+// Example of query string
 const queryStr = 'access_token=50d935da34d5bf48d0560e30d1f052ee56d030bc&scope=user%3Aemail&token_type=bearer'
 const queryStrToObject = (queryStr) => {
   const pairs = queryStr.split('&')
