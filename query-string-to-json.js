ac20c29c761d498f7c8d40553245ca5b562d8cb5
Manjunath Reddy <manju16832003@gmail.com>
2018-09-03 00:36:42 +0800

    Refactor to es6 style

---
+++ query-string-to-json.js
@@ -0,0 +1,15 @@
+const queryStr = 'access_token=50d935da34d5bf48d0560e30d1f052ee56d030bc&scope=user%3Aemail&token_type=bearer'
+const queryStrToObject = (queryStr) => {
+  const pairs = queryStr.split('&')
+  const response = {}
+
+  pairs.forEach(pair => {
+    pair = pair.split('=')
+
+    response[pair[0]] = decodeURIComponent(pair[1] || '')
+   })
+
+  return response
+}
+
+console.log(queryStrToObject(queryStr))
