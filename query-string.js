64d66c913a420ba80cb630f4c007a84b0260f774
Manjunath Reddy <manju16832003@gmail.com>
2018-09-03 00:46:08 +0800

    Update comments

--- query-string.js
+++ query-string.js
@@ -3,3 +3,11 @@ const queryStr = require('querystring')
 const query = 'access_token=50d935da34d5bf48d0560e30d1f052ee56d030bc&scope=user%3Aemail&token_type=bearer'
 
 console.log('Query => ', queryStr.parse(query))
+// Output
+/**
+ * { 
+ *    access_token: '50d935da34d5bf48d0560e30d1f052ee56d030bc',
+ *    scope: 'user:email',
+ *     token_type: 'bearer' 
+ * }
+ */
