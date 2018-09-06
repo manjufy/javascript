edef86c10ae2a46398e424beb6e4aeca9fff5bc6
Manjunath Reddy <manju16832003@gmail.com>
2018-09-07 00:05:48 +0800

    Destructuring

--- desctructuring/index.js
+++ desctructuring/index.js
@@ -8,3 +8,15 @@ console.log(c, d, rest) //10 11 [12, 13, 14]
 
 const [ amount = 0, total = 0] = [100]
 console.log(amount, total) // 100 0
+
+
+var e = 1;
+var f = 3;
+
+[e, f] = [f, e];
+console.log(e); // 3
+console.log(f); // 1
+
+// ignore return values
+const [l, , n] = [1, 2, 3]
+console(l, n)
