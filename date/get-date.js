67be4d6e14ab1901f80168ca534def2798862ac4
Manjunath Reddy <manju16832003@gmail.com>
2018-09-19 10:31:15 +0800

    JS Get Date in String format

--- date/get-date.js
+++ date/get-date.js
@@ -1,8 +1,8 @@
-const str = '2018-09-20T04:00:00.000Z'
-console.log('Date => ', getDate(str))
+const str = '2018-09-20'
+console.log('Date => ', getDate(str)) // 20 September 2018
 
 function getDate(date) {
     const newDate = new Date(date)
 
-    return newDate.getDay() + ' ' + newDate.toLocaleString('en-us', { month: "long"}) + ' ' + newDate.getFullYear()
+    return newDate.getDate() + ' ' + newDate.toLocaleString('en-us', { month: "long"}) + ' ' + newDate.getFullYear()
 }
