ae36481058cc589223c5e5af2fac32cfcbdccbbb
Manjunath Reddy <manju16832003@gmail.com>
2018-10-08 09:44:14 +0800

    JS Prototyping

--- prototype/inheritance.js
+++ prototype/inheritance.js
@@ -14,3 +14,6 @@ console.log(animal)
 console.log(animal.__proto__) // Not referenced to any object
 console.log(rabbit.__proto__) // Referenced to aninmal object
 console.log(rabbit)
+// We can find both properties in rabbit now
+console.log(rabbit.eats)
+console.log(rabbit.jumps)
