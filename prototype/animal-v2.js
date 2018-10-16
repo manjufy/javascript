cac0a950db63c607bdcc58f1e5c2641bb85ee90c
Manjunath Reddy <manju16832003@gmail.com>
2018-10-16 22:00:29 +0800

    Prototyping

---
+++ prototype/animal-v2.js
@@ -0,0 +1,29 @@
+// Functional Instantiation
+
+function Animal (name, energy) {
+    let animal = {}
+    animal.name = name
+    animal.energy = energy
+  
+    animal.eat = function (amount) {
+      console.log(`${this.name} is eating.`)
+      this.energy += amount
+    }
+  
+    animal.sleep = function (length) {
+      console.log(`${this.name} is sleeping.`)
+      this.energy += length
+    }
+  
+    animal.play = function (length) {
+      console.log(`${this.name} is playing.`)
+      this.energy -= length
+    }
+  
+    return animal
+}
+
+const tiger = Animal('Tiger', 10)
+const lion = Animal('Lion', 15)
+
+console.log('Tiger', tiger.play(5))
