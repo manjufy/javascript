0ae4022eb10da58a40c34d9b6f7430f7967547e7
Manjunath Reddy <manju16832003@gmail.com>
2018-10-22 22:13:44 +0800

    JS array loop

---
+++ for-loop/array-obj.js
@@ -0,0 +1,214 @@
+const list = [
+    [
+        {
+            "item_id": 1506
+        },
+        {
+            "item_id": 1508
+        },
+        {
+            "item_id": 1162
+        },
+        {
+            "item_id": 1103
+        },
+        {
+            "item_id": 1386
+        },
+        {
+            "item_id": 1165
+        },
+        {
+            "item_id": 989
+        },
+        {
+            "item_id": 1073
+        },
+        {
+            "item_id": 1494
+        },
+        {
+            "item_id": 1495
+        },
+        {
+            "item_id": 1456
+        },
+        {
+            "item_id": 1443
+        },
+        {
+            "item_id": 1491
+        },
+        {
+            "item_id": 952
+        },
+        {
+            "item_id": 1287
+        },
+        {
+            "item_id": 1061
+        },
+        {
+            "item_id": 1402
+        },
+        {
+            "item_id": 1485
+        },
+        {
+            "item_id": 1392
+        },
+        {
+            "item_id": 1325
+        },
+        {
+            "item_id": 1458
+        },
+        {
+            "item_id": 1029
+        },
+        {
+            "item_id": 1406
+        },
+        {
+            "item_id": 1465
+        },
+        {
+            "item_id": 1034
+        },
+        {
+            "item_id": 1255
+        },
+        {
+            "item_id": 1395
+        },
+        {
+            "item_id": 490
+        },
+        {
+            "item_id": 1323
+        },
+        {
+            "item_id": 1413
+        },
+        {
+            "item_id": 1427
+        },
+        {
+            "item_id": 1038
+        },
+        {
+            "item_id": 1428
+        },
+        {
+            "item_id": 1153
+        },
+        {
+            "item_id": 1420
+        },
+        {
+            "item_id": 1421
+        },
+        {
+            "item_id": 1422
+        },
+        {
+            "item_id": 1326
+        },
+        {
+            "item_id": 1328
+        },
+        {
+            "item_id": 1383
+        },
+        {
+            "item_id": 904
+        },
+        {
+            "item_id": 1448
+        },
+        {
+            "item_id": 1449
+        },
+        {
+            "item_id": 1358
+        },
+        {
+            "item_id": 1440
+        },
+        {
+            "item_id": 1320
+        },
+        {
+            "item_id": 1455
+        },
+        {
+            "item_id": 1318
+        },
+        {
+            "item_id": 1415
+        },
+        {
+            "item_id": 1400
+        },
+        {
+            "item_id": 1293
+        },
+        {
+            "item_id": 1341
+        },
+        {
+            "item_id": 1432
+        },
+        {
+            "item_id": 1503
+        },
+        {
+            "item_id": 1490
+        },
+        {
+            "item_id": 1411
+        },
+        {
+            "item_id": 1329
+        },
+        {
+            "item_id": 1408
+        },
+        {
+            "item_id": 1453
+        },
+        {
+            "item_id": 1027
+        },
+        {
+            "item_id": 1322
+        },
+        {
+            "item_id": 839
+        },
+        {
+            "item_id": 1493
+        },
+        {
+            "item_id": 1433
+        },
+        {
+            "item_id": 1452
+        },
+        {
+            "item_id": 1470
+        }
+    ]
+]
+
+const items = [1, 2, 3, 4]
+
+// list[0].forEach(item => {
+//     console.log(item)
+// })
+
+// for(val of list[0]) {
+//     console.log(val.item_id)
+// }
+
+console.log(typeof items)
