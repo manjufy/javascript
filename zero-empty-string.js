e04a3f662465da1b069e817120352b745700674f
Manjunath Reddy <manju16832003@gmail.com>
2018-10-30 12:02:13 +0800

    zero string comparision

---
+++ zero-empty-string.js
@@ -0,0 +1,23 @@
+const deposit = 0
+
+if (deposit == '') {
+    console.log('Deposit is empty!')
+}
+
+if (0 == deposit) {
+    console.log('We don\'t have money')
+}
+
+if ('0' == deposit) {
+    console.log('We don\'t have money')
+}
+
+if (deposit == '0') {
+    console.log('We don\'t have money')
+}
+
+console.log('toString()', deposit.toString())
+console.log('toString()', deposit.toString().length)
+
+const balance = ''
+console.log('balance toString()', balance.toString().length)
