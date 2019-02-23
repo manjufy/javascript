bfc1ea05cf8835156cc1ef569fb33e4ffe7ec61a
Manjunath Reddy <manju16832003@gmail.com>
2019-02-23 11:06:38 +0800

    Pickby native implementation over lodash

--- lodash-to-native/pickBy.js
+++ lodash-to-native/pickBy.js
@@ -3,11 +3,20 @@ const schema = {
     age: 0,
     address: '',
     phone_no: '',
+    active: false,
+    accounts: [{
+        id: 1
+    }]
 }
 
 const params = {
     name: 'Manjunath',
-    age: 30
+    age: 30,
+    phone_no: null,
+    active: true,
+    accounts: [{
+        id: 1
+    }]
 }
 
 function pickBy(object) {
