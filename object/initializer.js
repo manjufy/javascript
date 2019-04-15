c8b9381ce173c52d4c83860606151108c7f12a77
Manjunath Reddy <manju16832003@gmail.com>
2019-04-15 10:13:35 +0800

    Objects

---
+++ object/initializer.js
@@ -0,0 +1,12 @@
+var o = {};
+var o = {a: 'foo', b: 42, c: {}};
+
+var a = 'foo', b = 42, c = {};
+var o = {a: a, b: b, c: c};
+
+var o = {
+  property: function (parameters) {},
+  get property() {},
+  set property(value) {}
+};
+console.log(o);
---
+++ object/shorthand.js
@@ -0,0 +1,5 @@
+const a = 'food', b = 42, c = {};
+
+const obj = {a, b, c}
+
+console.log(obj);
