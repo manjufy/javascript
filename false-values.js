ed2eadb491eaba96b4a6e7850f051ba2f6138907
Manjunath Reddy <manju16832003@gmail.com>
2020-03-28 21:10:49 +0800

    Falsy values

---
+++ false-values.js
@@ -0,0 +1,15 @@
+/**
+ * Fasly values in Javascript
+*/
+
+const empty = '';
+const zero = 0;
+const objNull = null;
+const varUndefined = undefined;
+const varFalse = false;
+const varNaN = NaN;
+
+// how to check if a value is truthy or false
+const str = 'This is string';
+console.log('String: Truthy or false => ', !!str);
+console.log('String: Truthy or false => ', Boolean(str));
